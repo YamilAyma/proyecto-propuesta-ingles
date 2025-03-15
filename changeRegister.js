0a057e17e23915b4159f804556be2b59a4da5f50
Jean Piere <jeanpierefernandez80@gmail.com>
2025-03-15 17:21:46 -0500

    registro

---
+++ changeRegister.js
@@ -0,0 +1,35 @@
+// Registro
+
+const btnAlumno = $('#btnAlumno')
+const btnEmpresa = $('#btnEmpresa')
+const btnSalirAlumno = $('#btnSalirAlumno')
+const btnSalirEmpresa = $('#btnSalirEmpresa')
+
+const formAlumnos = $('#form_registrerAlumnos')
+const formEmpresas = $('#form_registrerEmpresas')
+const boxElegir = $('#boxElegir')
+
+
+btnAlumno.addEventListener('click',()=>{
+    boxElegir.style.display='none';
+    formAlumnos.style.display='flex';
+})
+
+
+btnEmpresa.addEventListener('click',()=>{
+    boxElegir.style.display='none';
+    formEmpresas.style.display='flex';
+})
+
+btnSalirAlumno.addEventListener('click', () => {
+    boxElegir.style.display = 'flex';
+    formAlumnos.style.display = 'none';
+    console.log('Saliendo del formulario de alumnos');
+});
+
+btnSalirEmpresa.addEventListener('click', () => {
+    boxElegir.style.display = 'flex';
+    formEmpresas.style.display = 'none';
+    console.log('Saliendo del formulario de empresas');
+});
+
