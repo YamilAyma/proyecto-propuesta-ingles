a97d67f2074b562ddef7708199c0ad0e64277033
Yamil Ayma <yamilaymadev@gmail.com>
2025-03-23 15:36:38 -0500

    [BUG] + [FEAT] Cambio de tema y colores para el tema oscuor

--- script.js
+++ script.js
@@ -9,16 +9,19 @@ function reemplazarUrl(nueva_ruta){
 
 }
 
-// Modo oscuro
-const theme = $('#theme');
+
 
 // POR DEFECTO LA PAGINA FUE ESTABLECIDA EN MODO OSCURO(PREFERENCIA DEL DESARROLLADOR JPFT)
 // Al momento de cargar la pagina se establece el modo que se guarda en el LocalStorage
 document.addEventListener("DOMContentLoaded", () => {
+    // Modo oscuro
+    const theme = $('#theme');
+    
+    console.log("COLOCANDO TEMA")
     const getTheme = localStorage.getItem('theme');         //Obtenemos el valor del item'Tema' que alberga que tema tiene
     if (getTheme === 'light') {                             //Condicional para establecer el tema
         document.body.classList.add('light-mode');
-    }else{
+    }else{  
         document.body.classList.remove('light-mode');
     }
 
@@ -31,13 +34,18 @@ document.addEventListener("DOMContentLoaded", () => {
             elemento.classList.toggle("invisible")
         }
     }
+
+
+    // Al hacer click al boton tema
+    // Se coloca aquí debido a la carga de la página en la redirección
+    theme.addEventListener('click',()=>{
+        console.log("CAMBIANDO TEMA")
+        const isTheme = document.body.classList.toggle('light-mode');   //Agrega o quita el tema oscuro o claro (si ya lo tiene lo quita - si no lo agrega)
+        localStorage.setItem('theme', isTheme ? 'light' : 'dark');      //Establece el nuevo valor del tema (oscuro o claro) segun el caso
+    })
 });
 
-// Al hacer click al boton tema
-theme.addEventListener('click',()=>{
-    const isTheme = document.body.classList.toggle('light-mode');   //Agrega o quita el tema oscuro o claro (si ya lo tiene lo quita - si no lo agrega)
-    localStorage.setItem('theme', isTheme ? 'light' : 'dark');      //Establece el nuevo valor del tema (oscuro o claro) segun el caso
-})
+
 
 
 const btnMenu = $('#btnMenu')
