862574626ef07c2118da9f19cd9756037610a6bb
Yamil Ayma <yamilaymadev@gmail.com>
2025-03-23 15:44:23 -0500

    Funcionalidad a botones y cierre de sesion

--- restriccionCuenta.js
+++ restriccionCuenta.js
@@ -1,8 +1,13 @@
 // Script para permitir que pueda acceder a su cuenta cuando termino de login/register
-
 // Redirigir si no se ha encontrado usuario
 const usuario = localStorage.getItem('usuario')
 if(!usuario){
     // Hacia el home
     reemplazarUrl("login.html")
+}
+
+function cerrarSesion(){
+    console.log("CERRAR")
+    localStorage.removeItem('usuario')
+    reemplazarUrl("index.html")
 }
